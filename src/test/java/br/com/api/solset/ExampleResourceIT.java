497263c9097fb5c26b75b9f797852efb905d6d96
Melqui <melki2011@gmail.com>
2022-07-06 21:10:27 -0300

    start project

---
+++ src/test/java/br/com/api/solset/ExampleResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.api.solset;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
